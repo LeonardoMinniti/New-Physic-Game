cbcdf64368978273a101258a3c086e0d8a33541d
LeonardoMinniti <33843867+LeonardoMinniti@users.noreply.github.com>
2018-07-10 10:38:16 +0200

    Initial commit

---
+++ Assets/Scripts/Player1Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Script : MonoBehaviour {
+
+    public Rigidbody2D rbPlayer1;
+
+    public GameObject player1;
+
+    public float speed;
+
+    public float gas;
+
+	void Start()
+    {
+        gas = 100f;
+
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+        bool inputKeyA = Input.GetKey("a");
+        bool inputKeyD = Input.GetKey("d");
+
+
+        if (inputKeyA && gas >= 0f)
+        {
+            gas -= gas * Time.deltaTime;
+
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+
+            float moveHorizontal = Input.GetAxis("Horizontal");
+
+            Vector2 movement = new Vector2(moveHorizontal, 0f);
+
+            rbPlayer1.AddForce(movement * speed);
+        }
+        else if(inputKeyD)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+            float moveHorizontal = Input.GetAxis("Horizontal");
+
+            Vector2 movement = new Vector2(moveHorizontal, 0f);
+
+            rbPlayer1.AddForce(movement * speed);
+        }
+        
+	}
+}
