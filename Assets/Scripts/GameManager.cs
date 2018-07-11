d4145d08cf49849ce54508bdcbd292f16c7ac981
LeonardoMinniti <33843867+LeonardoMinniti@users.noreply.github.com>
2018-07-11 19:22:26 +0200

    New UI, Bullet System etc.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public Rigidbody2D projectile;
+    private float windForce;
+    private Vector3 windDirection;
+    public static GameObject bullet;
+
+    private int round;
+
 	// Use this for initialization
 	void Start () {
+        round = 1;
+
+        windDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f) * 150, 0f, Random.Range(-1f, 1f) * 150);
+        bullet = transform.GetChild(0).gameObject;
+        projectile = bullet.GetComponent<Rigidbody2D>();
+
 		
 	}
 	
@@ -13,4 +26,11 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnTriggerStay2D(Collider2D collider2D)
+    {
+        Debug.Log("value is" + windForce);
+        Debug.Log(windDirection);
+        projectile.AddForce(windDirection * Time.deltaTime);
+    }
 }
---
+++ Assets/bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    public GameObject explosion;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        if (collision.transform.CompareTag("Player"))
+        {
+            //take health
+        } else if (collision.transform.CompareTag("MapDestructable"))
+        {
+            Destroy(collision.gameObject);
+        }
+        gameObject.SetActive(false);
+    }
+}
